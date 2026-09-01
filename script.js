b5f9278955b32393bd7939e45351ac107c2dfd22
Lucas Camilo <lucasboca2804@gmail.com>
2026-09-01 11:48:50 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("JavaScript funcionando!");
+const agendar = document.querySelector('.menu-btn');
+agendar.addEventListener('click', () => {
+    console.log("Agendamento iniciado");});
+    const agendar = document.querySelector('.btn-agendar');
+agendar.addEventListener('click', () => {
+    console.log("Agendamento iniciado");});
